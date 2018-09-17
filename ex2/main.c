bac01e92accf3256f811965596942edaa840908f
Jerker Bjorkqvist <jerker.bjorkqvist@abo.fi>
2018-09-17 22:25:32 +0300

    Blinking....

--- ex2/main.c
+++ ex2/main.c
@@ -2,18 +2,24 @@
 
 #include "led.h"
 #include "switch.h"
+#include "timer.h"
+
 
 void main() {
 
 	WDTCTL = WDTPW | WDTHOLD;		// Stop watchdog timer
+	
+    Led_Init(500);
+	Switch_Init();
+	Timer_Init();
+	_enable_interrupts();
 
-    
 	
 	while (1) {
-		
+		LPM1;
 		if (Switch_GetStat() == PRESSED) {
 			Led_Setstat(LEDSTAT_ON);
 		}
-		Led_Setstat(LEDSTAT_OFF);		
+		Led_Setstat(LEDSTAT_BLINK);		
 	}
 }
